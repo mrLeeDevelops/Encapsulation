e4a3cc569faf8820a53d0df4adaadd0fd3513d0b
mrLeeDevelops <mrlee.truecoder@gmail.com>
2021-08-10 11:23:57 -0500

    Completed Encapsulation Exercise

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+
+        }
+
+        private double Balance = 0;
+
+        public void Deposit(double dep)
+        {
+            Console.WriteLine("How much would you like to Deposit?");
+            dep = double.Parse(Console.ReadLine());
+            Console.WriteLine("THANK YOU FOR YOUR DEPOSIT!");
+            Balance += dep;
+        }
+
+        public void GetBalance()
+        {
+            Console.WriteLine($"YOUR BALANCE IS: ${Balance}.");
+        }
+
+
+    }
+}
+
